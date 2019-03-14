aed576c20cf32849db8c664a4196ec0adfaabd79
bhavyang <48353021+bhavyangbj@users.noreply.github.com>
2019-03-14 19:51:25 +0530

    Create prog_24.c

---
+++ prog_24.c
@@ -0,0 +1,30 @@
+// A program to check whether the given number is prime or not
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,flag=0;
+    printf("\n Enter a positive integer: ");
+    scanf("%d",&n);
+
+    for(i=2;i<=n/2;i++)
+    {
+        // condition for nonprime number
+        if(n%i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+    if (n == 1) 
+      printf("1 is neither a prime nor a composite number. \n");
+    else 
+    {
+        if (flag == 0)
+          printf("%d is a prime number.\n",n);
+        else
+          printf("%d is not a prime number.\n",n);
+    }    
+    getch();
+}
