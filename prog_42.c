0ce168d8a0ad2f82a0cb00174381ca2e04845c43
bhavyang <48353021+bhavyangbj@users.noreply.github.com>
2019-05-04 17:41:49 +0530

    Update prog_42.c

--- prog_42.c
+++ prog_42.c
@@ -1,17 +1,17 @@
 //Write a program that defines a function to add first n numbers.
 
 #include<stdio.h>
-void add_numer()
+int add_numer(int n,sum)
 {
- int n,sum;
- printf("\n Enter number : ");
- scanf("%d",&n);
- sum=(n*(n+1))/2;
- printf("\n Sum of the first %d numbers is : %d \n",n,sum);
+ sum=n*(n+1)/2;
+ return sum;
 }
 
 void main()
 {
- int n;
- add_numer();
+ int n,sum;
+ printf("\n Enter number : ");
+ scanf("%d",&n);
+ sum=add_numer(n,sum);
+ printf("\n Sum of the first %d numbers is : %d \n",n,sum);
 }
